77807909d252e05b17e0c5bebecc0a91ae9bc8e1
USUARIO08-PC\Usuario01 <bryan_Adri_94@outlook.com>
2016-11-23 11:41:09 -0500

    h

--- Proyecto2.py
+++ Proyecto2.py
@@ -1,20 +1,20 @@
-def creartxt(): 
-	archi=open('datos.txt','w') 
-	archi.close()
-def grabartxt(): 
-	archi=open('datos.txt','a') 
-	archi.write('HOLA\n') 
-	archi.write('PILATUÑA\n') 
-	archi.write('ESPAÑA\n') 
-	archi.close() 
+def creartxt():
+    archi=open('datos.txt','w')
+    archi.close()
+
+def grabartxt():
+    archi=open('datos.txt','a')
+    archi.write('BRYAN PILATUÑA\n')
+    archi.write('BRYAN ESPAÑA\n') 
+    archi.write('PROGRAMACION AVANZADA\n')
+    archi.close() 
 creartxt()
 grabartxt()
-def leertxt(): 
-	archi=open('datos.txt','r') 
-	linea=archi.readline() 
-	while linea!="": 
-                print(linea)
-                linea=archi.readline() 
-                archi.close()
 
-leertxt()
+def leertxt():
+    archi=open('datos.txt','r')
+    linea=archi.readline()
+    while linea!="":
+        print(linea)
+        linea=archi.readline()
+        archi.close()
