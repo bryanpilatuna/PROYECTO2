1c9ce6d3909debe28ae9835fdb74efa40edd8250
USUARIO07-PC\Usuario01 <byips1995@gamil.com>
2016-11-23 13:05:10 -0500

    siguiente

--- CONTEO.py
+++ CONTEO.py
@@ -2,23 +2,24 @@ def creartxt():
     archi=open('conteo.txt','w')
     archi.close()
     
-def grabartxt():
-    archi=open('datos.txt','a')
-    
+def grabartxt(a):
+    archi=open('conteo.txt','a')
+    archi.write("El total de las palabras son:\t")
+    archi.write(str(a))
     archi.close() 
-creartxt()
-grabartxt()
+
 
 def leertxt():
     archi=open('DOCUMENTO.txt','r')
     linea=archi.readline()
     while linea!="":
-        
         print(linea)
         a=len(linea)
         linea=archi.readline()
         print ("Los caracteres son son: \n")
         print (a)                
         archi.close()
-creartxt()
+    grabartxt(a)
+        
+creartxt()       
 leertxt()
