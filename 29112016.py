d41856e0f79eb455a3ee05811b4152aa4077af7d
DESKTOP-UBQC5LN\Bryan <byjps1995@gmai.com>
2016-11-29 13:56:11 -0500

    siguiente

--- 29112016.py
+++ 29112016.py
@@ -8,15 +8,31 @@ def grabartxt():
     archi.write(str())
     archi.close() 
 
-
-
 def leertxt():
+    h=0
+    p=0
+    a=''
     archi=open('harry.txt','r')
     linea=archi.readline()
     while linea!="":
         print(linea)
+        for i in range(len(linea)):
+            if linea[i] ==" ":
+                if a == 'Harry':
+                    h=h+1
+                elif a == 'Harry.':
+                    h=h+1
+                elif a == 'Potter':
+                    p=p+1
+                elif a == 'Potter.':
+                    p=p+1
+                a=''
+            else:
+                a=a+linea[i]
         linea=archi.readline()     
         archi.close()
+    print(h)
+    print(p)
     grabartxt()
     
         
